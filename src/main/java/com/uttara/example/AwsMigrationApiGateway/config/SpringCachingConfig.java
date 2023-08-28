e638b4b2944a283844b77f15c7c05d2aba42aab4
HBUm871 <umesha.kit@gmail.com>
2023-08-28 07:54:56 +0530

    commited cache config

---
+++ src/main/java/com/uttara/example/AwsMigrationApiGateway/config/SpringCachingConfig.java
@@ -0,0 +1,17 @@
+package com.uttara.example.AwsMigrationApiGateway.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class SpringCachingConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("deviceId","deviceEmailId","code","awsRoute","ngdcRoute");
+    }
+}
